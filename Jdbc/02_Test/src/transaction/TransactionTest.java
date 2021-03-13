47c9ec18ab2103ce1870ba29707708c2c41b711f
CYZ <1822550890@qq.com>
2021-03-13 16:38:21 +0000

    数据库连接的隔离级别设置

--- Jdbc/02_Test/src/transaction/TransactionTest.java
+++ Jdbc/02_Test/src/transaction/TransactionTest.java
@@ -1,11 +1,11 @@
 package transaction;
 
+import bean.User;
 import org.junit.Test;
 import util.JDBCUtils;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.lang.reflect.Field;
+import java.sql.*;
 
 /**
  * @author Chenyang
@@ -50,7 +50,7 @@ public class TransactionTest {
 
     //------------考虑数据库事务后的操作-------------
     @Test
-    public void testUpdateWithTx(){
+    public void testUpdateWithTx() {
         Connection connect = null;
         try {
             connect = JDBCUtils.getConnection();
@@ -104,5 +104,67 @@ public class TransactionTest {
             JDBCUtils.closeResource(null, ps);
         }
         return 0;
+    }
+
+    //**************************************************************
+    //查询
+    @Test
+    public void testTxSelect() throws Exception {
+        Connection connect = JDBCUtils.getConnection();
+        connect.setAutoCommit(false);
+
+        //获取当前连接的隔离级别
+        System.out.println(connect.getTransactionIsolation());
+        //设置当前连接的隔离级别
+        connect.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+
+        String sql = "select user,password,balance from user_table where user=?";
+        User user = getInstance(connect, User.class, sql, "AA");
+        System.out.println(user);
+    }
+
+    //提交
+    @Test
+    public void testTxUpdate() throws Exception {
+        Connection connect = JDBCUtils.getConnection();
+        connect.setAutoCommit(false);
+        String sql = "update user_table set balance = ? where user=?";
+        update(connect, sql, 2000, "AA");
+        Thread.sleep(15000);
+        System.out.println("修改结束");
+    }
+
+    //通用的查询操作，返回表中的一条记录---Version 2.0(考虑上事务)
+    public <T> T getInstance(Connection connect, Class<T> clazz, String sql, Object... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = connect.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int count = rsmd.getColumnCount();
+
+            if (rs.next()) {
+                T obj = clazz.newInstance();
+                for (int i = 0; i < count; ++i) {
+                    Object value = rs.getObject(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(obj, value);
+                }
+                return obj;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+        return null;
     }
 }
