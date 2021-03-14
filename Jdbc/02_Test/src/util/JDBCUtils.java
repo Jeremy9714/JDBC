cd21bd0841291f75486bb1e466a07826b7b4c277
CYZ <1822550890@qq.com>
2021-03-14 16:44:17 +0000

    DBUtils工具类，实现资源的关闭(close和closeQuietly方法)

--- Jdbc/02_Test/src/util/JDBCUtils.java
+++ Jdbc/02_Test/src/util/JDBCUtils.java
@@ -4,6 +4,7 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.dbcp.BasicDataSourceFactory;
+import org.apache.commons.dbutils.DbUtils;
 
 import javax.sql.DataSource;
 import java.io.InputStream;
@@ -85,6 +86,32 @@ public class JDBCUtils {
         return connect;
     }
 
+    //使用工具类DBUtils，实现资源的关闭
+    public static void closeResourceViaDBUtil(Connection connect, Statement ps, ResultSet rs){
+        //close()方法在方法内部判断参数对象是否为空
+//        try {
+//            DbUtils.close(rs);
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        try {
+//            DbUtils.close(ps);
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        try {
+//            DbUtils.close(connect);
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+
+        //closeQuietly方法在方法内处理异常
+        DbUtils.closeQuietly(rs);
+        DbUtils.closeQuietly(ps);
+        DbUtils.closeQuietly(connect);
+
+    }
+
     //关闭数据库连接和Statement
     public static void closeResource(Connection connect, Statement ps) {
         //资源关闭
@@ -123,4 +150,5 @@ public class JDBCUtils {
             throwables.printStackTrace();
         }
     }
+
 }
